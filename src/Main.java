cb5c27bb40bf1564cf8139c062726926739cb737
David John <djj@wfu.edu>
2021-04-07 14:23:19 -0400

    Improved input of state abbreviation and name. Now from a file, and setup() now does file input.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@
 //  David John
 //  April 2021
 
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 
 public class Main {
 
     // for testing search for these keys
-    private static final String []  mykeylist = {"CA", "VA", "NY", "AK", "TN", "WI", "PR", "CT"};
+    private static final String[] mykeylist = {"CA", "VA", "NY", "AK", "TN", "WI", "PR", "CT"};
 
     // testing routine
     public static void main(String[] args) {
@@ -22,67 +26,62 @@ public class Main {
     }
 
     // method to create and test StringMap
-    public static void StringMapExample(StringMap myStringMap){
+    public static void StringMapExample(StringMap myStringMap) {
 
         // create and initialize myStringMap for array based hashing
         //StringMap myStringMap = new ArrayStringMap();
         setup(myStringMap);
 
         System.out.println();
-        System.out.println("****  TEST OF "+myStringMap);
+        System.out.println("****  TEST OF " + myStringMap);
         System.out.println();
 
         double psum = 0.0;      // set initial number of probes
 
         // report number of probes for the testing keys
-        for (String mykey : mykeylist){
-            System.out.print(mykey+ " : ");
+        for (String mykey : mykeylist) {
+            System.out.print(mykey + " : ");
             String theValue = myStringMap.get(mykey);
-            System.out.printf("%15s",(theValue != null) ? theValue : "null");
-            System.out.println(" : "+myStringMap.getProbes());
+            System.out.printf("%15s", (theValue != null) ? theValue : "null");
+            System.out.println(" : " + myStringMap.getProbes());
             psum += myStringMap.getProbes();
         }
 
-        System.out.printf("%nTotal number of probes: %8.0f%n",psum);
-        System.out.printf("Average number of probes: %9.2f%n",psum/mykeylist.length);
+        System.out.printf("%nTotal number of probes: %8.0f%n", psum);
+        System.out.printf("Average number of probes: %9.2f%n", psum / mykeylist.length);
     }
 
 
+    // very exciting method to initialize myStringMap
+    private static void setup(StringMap myStringMap) {
 
-    private static void setup(StringMap myStringMap){
-        myStringMap.put("AK", "Alaska");
-        myStringMap.put("AL", "Alabama");
-        myStringMap.put("NC", "North Carolina");
-        myStringMap.put("CA", "California");
-        myStringMap.put("NY", "New York");
-        myStringMap.put("GA", "Georgia");
-        myStringMap.put("WY", "Wyoming");
-        myStringMap.put("MT", "Montana");
-        myStringMap.put("OR", "Oregon");
-        myStringMap.put("MA", "Massachusetts");
-        myStringMap.put("ME", "Maine");
-        myStringMap.put("RI", "Rhode Island");
-        myStringMap.put("WA", "Washington");
-        myStringMap.put("IA", "Iowa");
-        myStringMap.put("UT", "Utah");
-        myStringMap.put("CO", "Colorado");
-        myStringMap.put("IH", "Idaho");
-        myStringMap.put("NM", "New Mexico");
-        myStringMap.put("TX", "Texas");
-        myStringMap.put("KS", "Kansas");
-        myStringMap.put("TN", "Tennessee");
-        myStringMap.put("AR", "Arkansas");
-        myStringMap.put("IL", "Illinois");
-        myStringMap.put("IN", "Indiana");
-        myStringMap.put("NV", "Nevada");
-        myStringMap.put("SC", "South Carolina");
-        myStringMap.put("SD", "South Dakota");
-        myStringMap.put("ND", "North Dakota");
-        myStringMap.put("VA", "Virginia");
-        myStringMap.put("WI", "Wisconsin");
-        myStringMap.put("MD", "Maryland");
-        myStringMap.put("DE", "Delaware");
+        Scanner StateData = null;
+        try {
+            StateData = new Scanner(new FileReader("src/StateInfo.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Illegal input file name");
+            System.exit(77);
+        }
+
+        // grab data, first line is abbreviation, second is state name
+        while (StateData.hasNextLine()) {
+            String StateAbbrev= new String();
+            String StateName = new String();
+            try {
+                StateAbbrev = StateData.nextLine();
+                StateName = StateData.nextLine();
+            } catch (NoSuchElementException e){
+                System.out.println("Error in data file -- FIX IT!");
+                System.exit(66);
+            }
+
+            // remove (possible) spaces around StateAbbrev
+            myStringMap.put(StateAbbrev.strip(), StateName);
+        }
+
+        StateData.close();
+
+
     }
-
 
 }
